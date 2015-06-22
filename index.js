19ae56dfea99cf68eaeee10de72708b4e12b53c5
Charlie Cheever <ccheever@gmail.com>
2015-06-22 13:20:50 -0700

    Switching to use slugid
    This means less code and we use uuid v4 for randomness
    `crypto.randomBytes()` is probably fine, but this seems
    like a cleaner safer bet.

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
 var _ = require('lodash-node');
-var crypto = require('crypto');
+var slugid = require('slugid');
 
 var A_THOUSAND_YEARS_FROM_NOW = new Date(33000000000000); // (approximately...)
 
-var POW10 = Math.pow(2, 10); // 2 ^ 10
-var POW26 = Math.pow(2, 26); // 2 ^ 26
-
-function _newIdentifier(len) {
-  len = len || 24;
-
-  var b = crypto.randomBytes(len);
-
-  // This bit inspired by https://github.com/T-PWK/flake-idgen
-  var now = Date.now();
-  b.writeUInt8(Math.floor(now / 4) & 0xFF, 4);
-  b.writeUInt16BE(Math.floor(now / POW10) & 0xFFFF, 2);
-  b.writeUInt16BE(Math.floor(now / POW26) & 0xFFFF, 0);
-
-  return b.toString('base64');
-
+function _newIdentifier() {
+  return slugid.v4();
 }
 
 module.exports = function (opts) {
