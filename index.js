0a76a61633b88ef3732b6e6282fbd7a563ac0837
James Ide <ide@jameside.com>
2016-09-16 11:26:17 -0700

    [2.0.0] Update to be Koa 2 middleware, remove lodash dep, ES6

--- index.js
+++ index.js
@@ -1,55 +1,49 @@
-var _ = require('lodash-node');
-var slugid = require('slugid');
+const slugid = require('slugid');
 
-var A_THOUSAND_YEARS_FROM_NOW = new Date(33000000000000); // (approximately...)
+const A_THOUSAND_YEARS_FROM_NOW = new Date(33000000000000); // (approximately...)
 
 function _newIdentifier() {
   return slugid.v4();
 }
 
-module.exports = function (opts) {
-  opts = opts || {};
-
-  var sharedDefaults = {
+module.exports = function identify(opts = {}) {
+  let sharedDefaults = {
     overwrite: true,
     httpOnly: true,
     generateIdentifer: _newIdentifier,
   };
 
-  var sessionCookieOpts = _.assign({
+  let sessionCookieOpts = Object.assign({
     name: 'ks',
     prefix: 's-',
   }, sharedDefaults, opts, opts.session);
 
-  var browserCookieOpts = _.assign({
+  let browserCookieOpts = Object.assign({
     name: 'kb',
     expires: A_THOUSAND_YEARS_FROM_NOW,
     prefix: 'b-',
   }, sharedDefaults, opts, opts.browser);
 
-  return function* (next) {
-    var ks = this.cookies.get(sessionCookieOpts.name);
-    this.charlie = true;
+  return function(ctx, next) {
+    let ks = ctx.cookies.get(sessionCookieOpts.name);
     if (!ks) {
-      this.broad = true;
+      ctx.broad = true;
       ks = sessionCookieOpts.prefix + _newIdentifier();
-      this.sessionIdCreated = true;
-      this.cookies.set(sessionCookieOpts.name, ks, sessionCookieOpts);
+      ctx.sessionIdCreated = true;
+      ctx.cookies.set(sessionCookieOpts.name, ks, sessionCookieOpts);
     }
-    this.sessionId = ks;
+    ctx.sessionId = ks;
 
-    var kb = this.cookies.get(browserCookieOpts.name);
+    let kb = ctx.cookies.get(browserCookieOpts.name);
     if (!kb) {
       kb = browserCookieOpts.prefix + _newIdentifier();
-      this.browserIdCreated = true;
-      this.cookies.set(browserCookieOpts.name, kb, browserCookieOpts);
+      ctx.browserIdCreated = true;
+      ctx.cookies.set(browserCookieOpts.name, kb, browserCookieOpts);
     }
-    this.browserId = kb;
+    ctx.browserId = kb;
 
-    yield *next;
+    return next();
   };
-
 }
 
-module.exports._newIdentifier = _newIdentifier;
 module.exports.A_THOUSAND_YEARS_FROM_NOW = A_THOUSAND_YEARS_FROM_NOW;
